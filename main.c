983c951da9c2b6e6eb5f09aeb3e5f30689c6bf69
janettem <janette.metelinen@gmail.com>
2020-03-19 12:09:46 +0100

    Now input gets printed

--- main.c
+++ main.c
@@ -8,16 +8,20 @@ int main(int argc, char **argv)
 
     create_room_list(&rooms, &line);
     if (!(line))
-    handle_error();
-    handle_links(&links, line);
-    while (rooms != NULL)
+        handle_error();
+    links = get_links(line, rooms);
+    while (rooms)
     {
-        printf("%s |x %d|y %d! type %d\n", rooms->name, rooms->x, rooms->y, rooms->type);
+        if (rooms->type == 0)
+            printf("##start\n");
+        else if (rooms->type == 2)
+            printf("##end\n");
+        printf("%s %d %d\n", rooms->name, rooms->x, rooms->y);
         rooms = rooms->next;
     }
-        while (links != NULL)
+    while (links)
     {
-        printf("%s - %s\n", links->room1, links->room2);
+        printf("%s-%s\n", links->room1, links->room2);
         links = links->next;
     }
     return (0);
