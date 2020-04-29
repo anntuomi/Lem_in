4f3c3326a72f4eeda827fc8dc003ab0bb508e83e
janettem <janette.metelinen@gmail.com>
2020-04-29 14:18:11 +0100

    Optimized the algorithm

--- solve.c
+++ solve.c
@@ -1,45 +1,56 @@
 #include "lemin.h"
 
-static t_route	**assign_paths(t_route **ant, t_routes **ordered,
+static void		assign_paths(t_route **ant, t_routes **ordered,
 int path_count, int amount)
 {
-	int			i;
+	int			rooms;
+	int			moves;
+	int			i1;
+	int			i2;
 
-	i = 0;
-	while (i < amount)
+	i1 = 0;
+	rooms = 0;
+	while (i1 < path_count)
+		rooms += ordered[i1++]->rooms - 2;
+	moves = amount + rooms;
+	moves = !(moves % path_count) ? moves / path_count : moves / path_count + 1;
+	i1 = 0;
+	i2 = 0;
+	while (i1 < amount)
 	{
-		//printf("Ant number %d (Path_count: %d) Mod: %d\n", i, path_count,
-		//(i) % path_count);
-		ant[i] = ordered[(i) % path_count]->route;
-		i++;
+		if (i2 == path_count || ordered[i2]->rooms - 1 > moves)
+		{
+			i2 = 0;
+			moves--;
+		}
+		ant[i1++] = ordered[i2++]->route;
 	}
-	return (ant);
+	ant[i1] = NULL;
 }
 
-static void		move_ants(int amount, t_route **ant, t_routes *route_list)
+static void		move_ants(t_farm farm)
 {
+	int			first;
 	int			i;
-	int			moved;
-	t_routes	*route;
-	int			ant_nbr;
 
-	i = amount - 1;
-	ant_nbr = 1;
-	moved = 0;
-	while (i >= 0)
+	first = 1;
+	i = 0;
+	while (farm.ants[i])
 	{
-		if (ant[i]->room->type != END && (ant[i]->next->room->ant_count == 0 \
-		|| ant[i]->next->room->type == END))
+		if (farm.ants[i]->room->type != END &&
+		(!farm.ants[i]->next->room->ant_count
+		|| farm.ants[i]->next->room->type == END))
 		{
-			ant[i]->room->ant_count--;
-			ant[i] = ant[i]->next;
-			ant[i]->room->ant_count++;
-			moved == 1 ? printf(" L%d-%s", ant_nbr, ant[i]->room->name) : \
-			printf("L%d-%s", ant_nbr, ant[i]->room->name);
-			moved = 1;
+			farm.ants[i]->room->ant_count--;
+			farm.ants[i] = farm.ants[i]->next;
+			farm.ants[i]->room->ant_count++;
+			if (!first)
+				printf(" ");
+			else
+				first = 0;
+			printf("L%d-%s", i + 1, farm.ants[i]->room->name);
 		}
-		i--;
-		ant_nbr++;
+		i++;
 	}
 	printf("\n");
 }
@@ -62,38 +73,25 @@ int				find_longest_route(t_routes **used, int path_count)
 
 double			calculate_moves(t_routes **used, int path_count, int ant_amount)
 {
-	double	float_result;
-	double	last_result;
-	double	remaining_moves;
-	int		longest_route;
+	int			moves;
+	int			rooms;
+	int			i;
 
-	if (path_count > ant_amount)
-		path_count = ant_amount;
-	longest_route = find_longest_route(used, path_count);
-	//printf("Most rooms: %d extra ants: %d\n", used[path_count - 1]->rooms - 2, ant_amount % path_count);
-	float_result = (double)longest_route + \
-	((double)ant_amount / (double)path_count);
-	//printf("Calc: %d + (%d / %d) : ", used[path_count - 1]->rooms - 2, ant_amount, path_count);
-	if (ant_amount % path_count != 0)
-	{
-		remaining_moves = (double)used[path_count - 1]->rooms - 2;
-		path_count = ant_amount % path_count;
-		longest_route = find_longest_route(used, path_count);
-		last_result = ((double)longest_route) + 1;
-		//printf("Remainder: %f ", last_result - remaining_moves);
-		if (remaining_moves < last_result)
-			float_result = float_result + 1.0;
-	}
-	//printf("Moves: %f path_count %d\n", float_result, path_count);
-	return (float_result);
+	i = 0;
+	rooms = 0;
+	while (i < path_count)
+		rooms += used[i++]->rooms - 2;
+	moves = ant_amount + rooms;
+	moves = !(moves % path_count) ? moves / path_count : moves / path_count + 1;
+	return (moves);
 }
 
 int				adjust_to_ant_amount(int path_count, int ant_amount,
 t_routes **ordered, t_routes **used)
 {
-	double	moves;
-	int		new_path_count;
-	double	tested_moves;
+	int			moves;
+	int			new_path_count;
+	int			tested_moves;
 
 	if (ordered[0]->rooms == 2)
 		return (-1);
@@ -122,11 +120,13 @@ void			solve(t_farm farm, t_routes **ordered, int path_count)
 
 	used_routes = determine_used_routes(farm, &path_count, 0, 0);
 	order_routes(used_routes);
+	print_unique_routes(used_routes);
+	printf("\n");
 	assign_paths(farm.ants, used_routes, path_count, farm.amount);
 	moves = 0;
 	while (farm.end->ant_count != farm.amount)
 	{
-		move_ants(farm.amount, farm.ants, farm.routes);
+		move_ants(farm);
 		moves++;
 	}
 	printf("Moves: %d\n", moves);
